205957e7ced9b94a6407e3bb946f5a8cb4c17cb7
kuba-kowalczykowski-lli <kuba.kowalczykowski@llinformatics.com>
2022-07-27 15:48:13 +0200

    Generate board in loops

--- src/index.js
+++ src/index.js
@@ -31,23 +31,18 @@ class Board extends React.Component {
   }
 
   render() {
+    let squares_array = []
+    for (let i = 0; i < 3; i++) {
+      let squares_row = [];
+      for (let j = 0; j < 3; j++) {
+        squares_row.push(this.renderSquare(i * 3 + j));
+      }
+      squares_array.push(<div className="board-row" key={i}>{squares_row}</div>)
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {squares_array}
       </div>
     );
   }
